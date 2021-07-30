d296cdfbdeb817af1a077bcf3d9df2a95b28e727
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2021-07-30 22:52:25 +0900

    :memo: Add: ライセンスヘッダーの作成と修正

---
+++ resources/js/script.js
@@ -0,0 +1,12 @@
+/*-
+ProjectName: ValueAndValue
+FileName: index.html
+Encoding: UTF-8
+Author: TwoSquirrels
+CreationDate: Jul 30, 2021
+Copyright: (c) 2021 TwoSquirrels
+License:
+  name: Apache License, Version 2.0
+  url: https://apache.org/licenses/LICENSE-2.0
+-*/
+
